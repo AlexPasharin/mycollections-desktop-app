983e19df7243399f9be6f63d6d1432191413f936
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2025-07-18 20:43:30 +0300

    restore top level await

--- src/index.ts
+++ src/index.ts
@@ -36,28 +36,21 @@ const createWindow = async (): Promise<void> => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app
-  .whenReady()
-  .then(async () => {
-    ipcMain.handle("fetch-artists", (_, { cursor }: FetchArtistsParams) =>
-      fetchArtists({ cursor }),
-    );
+await app.whenReady().then(async () => {
+  ipcMain.handle("fetch-artists", (_, { cursor }: FetchArtistsParams) =>
+    fetchArtists({ cursor }),
+  );
 
-    await createWindow();
+  await createWindow();
 
-    app.on("activate", () => {
-      // On OS X it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) {
-        void createWindow();
-      }
-    });
-  })
-  .catch((error: unknown) => {
-    console.error(error);
-
-    process.exit(1);
+  app.on("activate", () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      void createWindow();
+    }
   });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
