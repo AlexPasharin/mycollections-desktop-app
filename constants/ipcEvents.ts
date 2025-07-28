e77b29b79be814a3a67a31dad69012ffe5c35d2c
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2025-07-28 17:42:29 +0300

    use constants for event names

---
+++ constants/ipcEvents.ts
@@ -0,0 +1,5 @@
+export const FETCH_ARTISTS = "fetch-artists";
+export const QUERY_ARTIST = "query-artist";
+
+export const OPEN_ARTISTS_LIST_WINDOW = "open-new-artists-list-window";
+export const OPEN_ARTIST_QUERY_WINDOW = "open-new-artist-query-window";
--- src/app/artists/query/preload.ts
+++ src/app/artists/query/preload.ts
@@ -2,8 +2,10 @@ import { contextBridge, ipcRenderer } from "electron";
 
 import type { API } from "./api";
 
+import { QUERY_ARTIST } from "@/constants/ipcEvents";
+
 const api = {
-  queryArtists: (query: string) => ipcRenderer.invoke("query-artists", query),
+  queryArtists: (query: string) => ipcRenderer.invoke(QUERY_ARTIST, query),
 } as const satisfies API;
 
 contextBridge.exposeInMainWorld("api", api);
