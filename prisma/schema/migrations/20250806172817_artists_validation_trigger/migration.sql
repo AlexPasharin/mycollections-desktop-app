7bfe0c3d8f1b6806653a3d1f7a00036fe44c108a
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2025-08-08 18:51:26 +0300

    update artists validation migration

--- prisma/schema/migrations/20250806172817_artists_validation_trigger/migration.sql
+++ prisma/schema/migrations/20250806172817_artists_validation_trigger/migration.sql
@@ -1,5 +1,20 @@
 -- This migration had to be generated empty with --create-only command and populated manually, because it's content cannot be expressed in Prisma schema
 
+CREATE OR REPLACE FUNCTION add_query_id(text TEXT)
+RETURNS TEXT AS $$
+DECLARE
+	query_id TEXT;
+BEGIN
+	query_id := current_setting('app.query_id', true);
+
+	IF query_id IS NULL OR query_id = '' THEN
+		RETURN text;
+	END IF;
+
+	RETURN format('id:%s:%s', query_id, text);
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION validate_artist()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -8,26 +23,35 @@ DECLARE
 BEGIN
 	name_trimmed := TRIM(NEW.name);
 
-    IF NEW.name IS DISTINCT FROM TRIM(name_trimmed) THEN
-        RAISE NOTICE 'Automatically trimmed leading/trailing spaces from "name" of artist "%". Original: "%", Corrected: "%"',
-			 NEW.artist_id,
-			 NEW.name,
-			 TRIM(name_trimmed);
+    IF NEW.name IS DISTINCT FROM name_trimmed THEN
+        RAISE NOTICE '%',
+			add_query_id(
+				format(
+					'Automatically trimmed leading/trailing spaces from "name" of artist "%s". Original: "%s", Corrected: "%s"',
+				 	NEW.artist_id,
+				 	NEW.name,
+				 	name_trimmed
+				)
+			);
 
-		NEW.name := TRIM(name_trimmed);
+		NEW.name := name_trimmed;
     END IF;
 
 	IF NEW.name_for_sorting IS NOT NULL THEN
 		name_for_sorting_trimmed := TRIM(NEW.name_for_sorting);
 
 		IF NEW.name_for_sorting IS DISTINCT FROM name_for_sorting_trimmed THEN
-			RAISE NOTICE
-				'Automatically trimmed leading/trailing spaces from "name for sorting" of artist "%". Original: "%", Corrected: "%"',
-				 NEW.artist_id,
-				 NEW.name_for_sorting,
-				 name_for_sorting_trimmed;
+			RAISE NOTICE '%',
+				add_query_id(
+					format(
+						'Automatically trimmed leading/trailing spaces from "name for sorting" of artist "%s". Original: "%s", Corrected: "%s"',
+					 	NEW.artist_id,
+					 	NEW.name_for_sorting,
+					 	name_for_sorting_trimmed
+					)
+				);
 
-			NEW.name_for_sorting := TRIM(name_for_sorting_trimmed);
+			NEW.name_for_sorting := name_for_sorting_trimmed;
 		END IF;
 	END IF;
 
