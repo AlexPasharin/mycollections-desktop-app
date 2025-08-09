301ac66a61c3bea5249feaa22458a4705d887484
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2025-08-09 14:24:14 +0300

    Feat/artists db triggers (#23)
    * add validate_artists trigger
    
    * gitignore scripts folder
    
    * update artists validation migration
    
    * migrations and schema changes
    
    * exclude "scripts" in tsconfig
    
    * cosmetic changes in artists schema
    
    * update kysely client with db notice listener possibility
    
    * update withDBMessanger to accept multiple queries to be executes in a transaction

---
+++ prisma/schema/migrations/20250806172817_artists_validation_trigger/migration.sql
@@ -0,0 +1,65 @@
+-- This migration had to be generated empty with --create-only command and populated manually, because it's content cannot be expressed in Prisma schema
+
+CREATE OR REPLACE FUNCTION add_query_id(text TEXT)
+RETURNS TEXT AS $$
+DECLARE
+	query_id TEXT;
+BEGIN
+	query_id := current_setting('app.query_id', true);
+
+	IF query_id IS NULL OR query_id = '' THEN
+		RETURN text;
+	END IF;
+
+	RETURN format('id:%s:%s', query_id, text);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION validate_artist()
+RETURNS TRIGGER AS $$
+DECLARE
+	name_trimmed TEXT;
+	name_for_sorting_trimmed TEXT;
+BEGIN
+	name_trimmed := TRIM(NEW.name);
+
+    IF NEW.name IS DISTINCT FROM name_trimmed THEN
+        RAISE NOTICE '%',
+			add_query_id(
+				format(
+					'Automatically trimmed leading/trailing spaces from "name" of artist "%s". Original: "%s", Corrected: "%s".',
+				 	NEW.artist_id,
+				 	NEW.name,
+				 	name_trimmed
+				)
+			);
+
+		NEW.name := name_trimmed;
+    END IF;
+
+	IF NEW.name_for_sorting IS NOT NULL THEN
+		name_for_sorting_trimmed := TRIM(NEW.name_for_sorting);
+
+		IF NEW.name_for_sorting IS DISTINCT FROM name_for_sorting_trimmed THEN
+			RAISE NOTICE '%',
+				add_query_id(
+					format(
+						'Automatically trimmed leading/trailing spaces from "name for sorting" of artist "%s". Original: "%s", Corrected: "%s".',
+					 	NEW.artist_id,
+					 	NEW.name_for_sorting,
+					 	name_for_sorting_trimmed
+					)
+				);
+
+			NEW.name_for_sorting := name_for_sorting_trimmed;
+		END IF;
+	END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER validate_artists
+BEFORE INSERT OR UPDATE OF name, name_for_sorting ON artists
+FOR EACH ROW
+EXECUTE FUNCTION validate_artist();
--- src/db/client/kysely.ts
+++ src/db/client/kysely.ts
@@ -1,17 +1,81 @@
-import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely";
+import { CamelCasePlugin, Kysely, PostgresDialect, sql } from "kysely";
 import { Pool } from "pg";
+import { v4 as uuidv4 } from "uuid";
+
+import { EventEmitter } from "node:events";
 
 import type { DB } from "@/types/db/database";
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    connectionString: process.env["DATABASE_URL"],
-  }),
+const pool = new Pool({
+  connectionString: process.env["DATABASE_URL"],
+});
+
+const DBMessenger = new EventEmitter();
+
+const noticeEvent = "DB notice";
+
+pool.on("connect", (client) => {
+  client.on("notice", ({ message }) => {
+    DBMessenger.emit(noticeEvent, message);
+  });
 });
 
 const client = new Kysely<DB>({
-  dialect,
+  dialect: new PostgresDialect({
+    pool,
+  }),
   plugins: [new CamelCasePlugin()],
 });
 
 export default client;
+
+const noticeMessageWithQueryIdRegEx = /^id:(?<queryId>[^:]+):(?<notice>.+)$/;
+
+export type QueryBuilder<T> = (kyselyClient: Kysely<DB>) => {
+  execute: () => Promise<T>;
+};
+
+/**
+ * Executes given queries (or one query) inside a transaction (in order given by array of queries) listening to notifications that DB raises with RAISE NOTICE
+ * Also accepts just one query, which is treated as an array of one value only
+ * Assumes notifications we want to catch are constructed using "add_query_id" function defined in the database (see migration "20250806172817_artists_validation_trigger")
+ *
+ * @param queries array of functions, each function takes kysely client and returns executable kysely query with result of type T. Alternatively can be a single function
+ * @returns result of query execution and array of caught notifications packed in an object
+ */
+export const withDBMessenger = async <T>(
+  queries: QueryBuilder<T> | QueryBuilder<T>[],
+): Promise<{ results: T[]; notifications: string[] }> => {
+  const queryId = uuidv4();
+  const notifications: string[] = [];
+
+  const noticeListener = (msg?: string) => {
+    const match = msg?.match(noticeMessageWithQueryIdRegEx);
+
+    // to identify notifications that come from execution of this query
+    if (match?.groups?.["queryId"] === queryId) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
+      notifications.push(match.groups["notice"] as string); // "as" assertion is safe by construction of regex
+    }
+  };
+
+  DBMessenger.on(noticeEvent, noticeListener);
+
+  if (!Array.isArray(queries)) {
+    queries = [queries];
+  }
+
+  const results: T[] = [];
+
+  await client.transaction().execute(async (trx) => {
+    await sql`SET app.query_id = ${sql.raw(`'${queryId}'`)}`.execute(trx);
+
+    for (const query of queries) {
+      results.push(await query(trx).execute());
+    }
+  });
+
+  DBMessenger.off(noticeEvent, noticeListener);
+
+  return { results, notifications };
+};
