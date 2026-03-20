13ff0e2fc56db9fe1bed2204565780a04f102f8d
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2026-03-20 22:29:31 +0200

    migrate to new field

---
+++ prisma/schema/migrations/20260320202714_remove_alt_name_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `release_alternative_name` on the `musical_releases` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "musical_releases" DROP COLUMN "release_alternative_name";
