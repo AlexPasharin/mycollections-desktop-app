cef53e4edebb3177aff99a23024434b770d168b1
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2026-03-30 21:08:49 +0300

    add formats to getReleaseById

--- src/db/releases/index.ts
+++ src/db/releases/index.ts
@@ -1,9 +1,30 @@
+import { sql } from "kysely";
+
 import client from "../client/kysely";
 import { aggregateDistinctValuesToArray } from "../utils";
 
-import type { GetReleaseById } from "@/types/releases";
+import type {
+  GetReleaseById,
+  ReleaseFormatOfReleaseItem,
+} from "@/types/releases";
 
 export { getEntryReleases } from "./entryReleases";
+
+const musicalReleaseColumns = [
+  "musicalReleases.releaseId",
+  "musicalReleases.releaseVersion",
+  "musicalReleases.releaseDate",
+  "musicalReleases.discogsUrl",
+  "musicalReleases.countries",
+  "musicalReleases.catalogueNumbers",
+  "musicalReleases.matrixRunout",
+  "musicalReleases.comment",
+  "musicalReleases.conditionProblems",
+  "musicalReleases.partOfQueenCollection",
+  "musicalReleases.relationToQueen",
+  "musicalReleases.releaseAlternativeNameId",
+  "musicalReleases.entryId",
+] as const;
 
 export const getReleaseById: GetReleaseById = (releaseId) =>
   client
@@ -14,36 +35,31 @@ export const getReleaseById: GetReleaseById = (releaseId) =>
       "musicalReleasesTags.releaseId",
     )
     .leftJoin("tags", "musicalReleasesTags.tagId", "tags.tagId")
+    .leftJoin(
+      "formatsOfReleases",
+      "musicalReleases.releaseId",
+      "formatsOfReleases.releaseId",
+    )
+    .leftJoin(
+      "releasesFormats",
+      "formatsOfReleases.formatId",
+      "releasesFormats.formatId",
+    )
     .where("musicalReleases.releaseId", "=", releaseId)
     .select([
-      "musicalReleases.releaseId",
-      "musicalReleases.releaseVersion",
-      "musicalReleases.releaseDate",
-      "musicalReleases.discogsUrl",
-      "musicalReleases.countries",
-      "musicalReleases.catalogueNumbers",
-      "musicalReleases.matrixRunout",
-      "musicalReleases.comment",
-      "musicalReleases.conditionProblems",
-      "musicalReleases.partOfQueenCollection",
-      "musicalReleases.relationToQueen",
-      "musicalReleases.releaseAlternativeNameId",
-      "musicalReleases.entryId",
+      ...musicalReleaseColumns,
       aggregateDistinctValuesToArray("tags.tag", "tags"),
+      sql<ReleaseFormatOfReleaseItem[]>`coalesce(
+        jsonb_agg(DISTINCT jsonb_build_object(
+          'id', ${sql.ref("formatsOfReleases.id")},
+          'jukeboxHole', ${sql.ref("formatsOfReleases.jukeboxHole")},
+          'pictureSleeve', ${sql.ref("formatsOfReleases.pictureSleeve")},
+          'speed', ${sql.ref("formatsOfReleases.speed")},
+          'amount', ${sql.ref("formatsOfReleases.amount")},
+          'shortName', ${sql.ref("releasesFormats.shortName")},
+        )) FILTER (WHERE ${sql.ref("formatsOfReleases.id")} IS NOT NULL),
+        '[]'::jsonb
+      )`.as("formats"),
     ])
-    .groupBy([
-      "musicalReleases.releaseId",
-      "musicalReleases.releaseVersion",
-      "musicalReleases.releaseDate",
-      "musicalReleases.discogsUrl",
-      "musicalReleases.countries",
-      "musicalReleases.catalogueNumbers",
-      "musicalReleases.matrixRunout",
-      "musicalReleases.comment",
-      "musicalReleases.conditionProblems",
-      "musicalReleases.partOfQueenCollection",
-      "musicalReleases.relationToQueen",
-      "musicalReleases.releaseAlternativeNameId",
-      "musicalReleases.entryId",
-    ])
+    .groupBy(musicalReleaseColumns)
     .executeTakeFirst();
