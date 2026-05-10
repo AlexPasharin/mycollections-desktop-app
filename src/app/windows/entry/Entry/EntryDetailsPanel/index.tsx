f17432d9487f4297bb2031805a4218ceb0fdd6bf
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2026-05-10 16:37:06 +0300

    refactor

--- src/app/windows/entry/Entry/EntryDetailsPanel/index.tsx
+++ src/app/windows/entry/Entry/EntryDetailsPanel/index.tsx
@@ -10,89 +10,102 @@ type EntryDetailsPanelProps = {
   entry: EntryByIdResult;
 };
 
-const EntryDetailsPanel: FC<EntryDetailsPanelProps> = ({ entry }) => (
-  <div className={styles.entryPanel}>
-    <div className={styles.field}>
-      {entry.types.length > 0 ? (
-        <ul className={styles.typesList}>
-          {entry.types.map((typeName) => (
-            <li key={typeName} className={styles.typesListItem}>
-              {typeName}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className={styles.italicNote}>
-          (Types of this entry are not known, please update types of this entry
-          in the database)
+const EntryDetailsPanel: FC<EntryDetailsPanelProps> = ({ entry }) => {
+  const {
+    types,
+    altNames,
+    originalReleaseDate,
+    discogsUrl,
+    partOfQueenCollection,
+    relationToQueen,
+    tags,
+    comment,
+  } = entry;
+
+  return (
+    <div className={styles.entryPanel}>
+      <div className={styles.field}>
+        {types.length > 0 ? (
+          <ul className={styles.typesList}>
+            {types.map((typeName) => (
+              <li key={typeName} className={styles.typesListItem}>
+                {typeName}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className={styles.italicNote}>
+            (Types of this entry are not known, please update types of this
+            entry in the database)
+          </p>
+        )}
+      </div>
+
+      {altNames.length > 0 && (
+        <p className={styles.field}>
+          <span className={styles.fieldLabel}>Also known as: </span>
+          {altNames.map(({ name }) => name).join(", ")}
         </p>
       )}
-    </div>
 
-    {entry.altNames.length > 0 && (
-      <p className={styles.field}>
-        <span className={styles.fieldLabel}>Also known as: </span>
-        {entry.altNames.map(({ name }) => name).join(", ")}
-      </p>
-    )}
+      <div className={styles.field}>
+        <span className={styles.fieldLabel}>Original release date: </span>
+        {originalReleaseDate === null ? (
+          "(Unknown)"
+        ) : "error" in originalReleaseDate ? (
+          <DataWithErrorDisplay
+            value={originalReleaseDate.value}
+            error={originalReleaseDate.error}
+          />
+        ) : (
+          formatGeneralizedDate(originalReleaseDate)
+        )}
+      </div>
 
-    <div className={styles.field}>
-      <span className={styles.fieldLabel}>Original release date: </span>
-      {entry.originalReleaseDate === null ? (
-        "(Unknown)"
-      ) : "error" in entry.originalReleaseDate ? (
-        <DataWithErrorDisplay
-          value={entry.originalReleaseDate.value}
-          error={entry.originalReleaseDate.error}
-        />
-      ) : (
-        formatGeneralizedDate(entry.originalReleaseDate)
+      {discogsUrl && (
+        <p className={styles.field}>
+          <span className={styles.fieldLabel}>Discogs url: </span>
+          <a href={discogsUrl} target="_blank" rel="noreferrer">
+            {discogsUrl}
+          </a>
+        </p>
+      )}
+
+      {partOfQueenCollection && (
+        <p className={styles.field}>
+          <span className={styles.fieldLabelItalic}>
+            Part of Queen collection
+          </span>
+        </p>
+      )}
+
+      {relationToQueen && (
+        <p className={styles.field}>
+          <span className={styles.fieldLabel}>Relation to Queen: </span>
+          {relationToQueen}
+        </p>
+      )}
+
+      {tags.length > 0 && (
+        <div className={styles.field}>
+          <span className={styles.fieldLabel}>Tags:</span>
+          <ul className={styles.typesList}>
+            {tags.map((tag) => (
+              <li key={tag} className={styles.tagsListItem}>
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {comment && (
+        <div>
+          <p className={styles.comment}>{comment}</p>
+        </div>
       )}
     </div>
-
-    {entry.discogsUrl && (
-      <p className={styles.field}>
-        <span className={styles.fieldLabel}>Discogs url: </span>
-        <a href={entry.discogsUrl} target="_blank" rel="noreferrer">
-          {entry.discogsUrl}
-        </a>
-      </p>
-    )}
-
-    {entry.partOfQueenCollection && (
-      <p className={styles.field}>
-        <span className={styles.fieldLabelItalic}>
-          Part of Queen collection
-        </span>
-      </p>
-    )}
-
-    {entry.relationToQueen && (
-      <p className={styles.field}>
-        <span className={styles.fieldLabel}>Relation to Queen: </span>
-        {entry.relationToQueen}
-      </p>
-    )}
-
-    {entry.tags.length > 0 && (
-      <div className={styles.field}>
-        <span className={styles.fieldLabel}>Tags:</span>
-        <ul className={styles.typesList}>
-          {entry.tags.map((tag) => (
-            <li key={tag} className={styles.tagsListItem}>
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-    )}
-
-    {entry.comment && (
-      <div>
-        <p className={styles.comment}>{entry.comment}</p>
-      </div>
-    )}
-  </div>
-);
+  );
+};
 
 export default EntryDetailsPanel;
