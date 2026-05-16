e042cdef4397ea5064e8c3dab8369c8fe3259998
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2026-05-16 20:26:48 +0300

    new db index (#131)

--- src/app/windows/main/api.ts
+++ src/app/windows/main/api.ts
@@ -1,14 +1,10 @@
 import type { FetchArtists, QueryArtist } from "@/types/artists";
-import type {
-  CreateArtistWindowParams,
-  CreateEntryWindowParams,
-} from "@/types/entries";
+import type { CreateArtistWindowParams } from "@/types/entries";
 
 export type API = {
   fetchArtists: FetchArtists;
   queryArtists: QueryArtist;
   openNewArtistWindow: (params: CreateArtistWindowParams) => void;
-  openNewEntryWindow: (params: CreateEntryWindowParams) => void;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
