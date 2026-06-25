4f8d560e711e97859021de1a0272367a2e0f7274
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2026-06-25 20:52:20 +0300

    better implementation of get artist by id

--- src/db/artists/artistById.ts
+++ src/db/artists/artistById.ts
@@ -1,32 +1,52 @@
-import type { Kysely } from "kysely";
+import { sql, type Kysely } from "kysely";
 
 import { dbClient } from "../client/kysely";
 
-import type { ArtistByIdResult, GetArtistById } from "@/types/artists";
+import type {
+  ArtistAltNameInfo,
+  ArtistByIdResult,
+  GetArtistById,
+} from "@/types/artists";
 import type { DB } from "@/types/db/database";
 
 export const fetchArtistByIdResult = async (
   db: Kysely<DB>,
   artistId: string,
 ): Promise<ArtistByIdResult | undefined> => {
+  const artistTableFields = [
+    "artists.artistId",
+    "artists.name",
+    "artists.nameForSorting",
+    "artists.type",
+    "artists.partOfQueenFamily",
+  ] as const;
+
   const artist = await db
     .selectFrom("artists")
-    .select(["artistId", "name", "nameForSorting", "type", "partOfQueenFamily"])
-    .where("artistId", "=", artistId)
+    .leftJoin(
+      "alternativeArtistNames",
+      "artists.artistId",
+      "alternativeArtistNames.artistId",
+    )
+    .where("artists.artistId", "=", artistId)
+    .select([
+      ...artistTableFields,
+
+      sql<ArtistAltNameInfo[]>`coalesce(
+        jsonb_agg(
+          jsonb_build_object(
+            'nameId', ${sql.ref("alternativeArtistNames.nameId")},
+            'name', ${sql.ref("alternativeArtistNames.name")}
+          )
+          ORDER BY ${sql.ref("alternativeArtistNames.name")}
+        ) FILTER (WHERE ${sql.ref("alternativeArtistNames.nameId")} IS NOT NULL),
+        '[]'::jsonb
+      )`.as("altNames"),
+    ])
+    .groupBy(artistTableFields)
     .executeTakeFirst();
 
-  if (!artist) {
-    return undefined;
-  }
-
-  const altNames = await db
-    .selectFrom("alternativeArtistNames")
-    .select(["nameId", "name"])
-    .where("artistId", "=", artistId)
-    .orderBy("name", "asc")
-    .execute();
-
-  return { ...artist, altNames };
+  return artist;
 };
 
 export const getArtistById: GetArtistById = (artistId, dbSource) =>
